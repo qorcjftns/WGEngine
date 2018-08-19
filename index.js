6f85c987e4da1985a3f2625158451310c58b4e77
qorcjftns <qorcjftns2@gmail.com>
2018-08-19 14:39:33 +0900

    fixed

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ var app 	= express();
 var http 	= require('http').Server(app);
 var io 		= require('socket.io')(http);
 
-app.get('/', function(Req, res) {
+app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/html/index.html');
 });
-app.use(express.static('public'));
+app.use('/static', express.static('static'));
 
 io.on('connection', function(socket) {
 	console.log('user connected');
