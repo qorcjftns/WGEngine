a3b7b7238cbb01d57c3a2e29341d0c472db3d911
qorcjftns <qorcjftns2@gmail.com>
2018-08-19 15:24:55 +0900

    postgresql

--- index.js
+++ index.js
@@ -4,11 +4,29 @@
 // Setting
 var PORT = process.env.PORT || 8080;
 
+// PostgreSQL
+const { Client } = require('pg');
+const client = new Client({
+	connectionString: process.env.DATABASE_URL,
+	ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT * FROM USER;', function(err, res) {
+	if(err) throw err;
+	for(let row of res.rows) {
+		console.log(JSON.stringify(row));
+	}
+	client.end();
+});
+
 // Create HTTP Web Server
 var express	= require('express');
 var app 	= express();
 var http 	= require('http').Server(app);
 var io 		= require('socket.io')(http);
+
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/html/index.html');
